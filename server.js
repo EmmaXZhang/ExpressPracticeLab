1d3e62bb7e25277356e70f72495aaa1c0df140cf
EmmaZhang <emmazxd@gmail.com>
2024-02-19 19:37:20 +1100

    fix res.render bug

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const studentDb = require("./data/student");
 //create app
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(3000, function () {
+  console.log("listening on port 3000");
+});
+
 //mount route
 app.get("/students-database", function (req, res) {
   const students = studentDb.getAll();
   //using student.ejs format
-  res.render("view/student", { students });
-});
-
-app.set("view engine", "ejs");
-app.set("view", path.join(__dirname, "views"));
-
-app.listen(3000, function () {
-  console.log("listening on port 3000");
+  res.render("student", { students });
 });
